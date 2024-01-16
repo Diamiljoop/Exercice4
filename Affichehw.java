fa5f6f4e797a9c24bc463141add998aafa20ad64
Diamiljoop <152229001+Diamiljoop@users.noreply.github.com>
2024-01-16 22:25:03 +0100

    Add files via upload

---
+++ Affichehw.java
@@ -0,0 +1,13 @@
+class Affichehw{
+
+    public void afficheobjet(){
+        System.out.println("Hello world");
+    }
+
+    public static void main(String [] args){
+      Affichehw obt1=new Affichehw();
+      obt1.afficheobjet();
+
+
+    }
+}
---
+++ Affichehwdeux.java
@@ -0,0 +1,18 @@
+class Affichehwdeux{
+
+  
+    public void m(){
+        System.out.println("Hello world");
+    }
+
+    public static void main(String [] args){
+      Affichehwdeux obj1=new Affichehwdeux();
+      Affichehwdeux obj2=new Affichehwdeux();
+      obj1.m();
+      obj2.m();
+
+      
+
+
+    }
+}
